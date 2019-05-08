2109144248913b54795ce64275f9fee1f2faded2
Roman <rgrabelnikov@yandex.ru>
2019-05-08 09:47:36 +0700

    Created form

---
+++ WeaponDesktop/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WeaponDesktop
+{
+    partial class WeaponsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeaponsForm));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.SumLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            // 
+            // AddBtn
+            // 
+            resources.ApplyResources(this.AddBtn, "AddBtn");
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // EditBtn
+            // 
+            resources.ApplyResources(this.EditBtn, "EditBtn");
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            resources.ApplyResources(this.DeleteBtn, "DeleteBtn");
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // SumLbl
+            // 
+            resources.ApplyResources(this.SumLbl, "SumLbl");
+            this.SumLbl.Name = "SumLbl";
+            // 
+            // WeaponsForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SumLbl);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "WeaponsForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Label SumLbl;
+    }
+}
+
